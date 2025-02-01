feb92475f1403bc019dc13c62936774d27aeae2c
AcherPlus <enmanuel.obando@unmsm.edu.pe>
2025-02-01 11:13:47 -0500

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { NotaTotalComponent } from './nota-total/nota-total.component';
 import { NotaIndividualComponent } from './nota-individual/nota-individual.component';
 
 export const routes: Routes = [
-    { path: 'inicio', component: InicioComponent },
+    { path: '', component: InicioComponent },
     { path: 'nota-total', component: NotaTotalComponent },
     { path: 'nota-individual', component: NotaIndividualComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'inicio'}
